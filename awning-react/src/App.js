565fb2cd1c172541b627ae4585357bbed96700ab
ninaduric <ninaduric464@gmail.com>
2024-06-09 17:34:02 +0200

    uređen navbar

--- awning-react/src/App.js
+++ awning-react/src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar';
+import {NavBar, NavBarLoggedIn} from './components/NavBar';
+
 import Home from './components/Home';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
 import WindGraph from './components/WindGraph';
 import Users from './components/User';
+import LogOut from './components/LogOut';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect, getUsers } from 'react';
+//import * as firebase from 'firebase';
+//import NavBarLoggedIn from "./components/NavBarLoggedIn";
+
 
 function App() {
+    const auth = getAuth();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const authStatus = auth.currentUser;
+    
+    useEffect(()=>{
+        auth.onAuthStateChanged(function(user) {
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+        })
+  },[])
+
+
+  
     return (
+        
         <Router>
             <div className="App">
-                <NavBar />
+                
+                {isAuthenticated===true ? <NavBarLoggedIn/> : <NavBar/>}
+                
+                
+                
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/login" element={<LogIn />} />
+                    <Route path="/logout" element={<LogOut />} />
                     <Route path="/wind-graph" element={<WindGraph />} />
-                    <Route path="/user/:userId" element={<Users />} />
+                    <Route path="/user" element={<Users />} />
                 </Routes>
                 <Footer />
             </div>
--- awning-react/src/components/LogOut.js
+++ awning-react/src/components/LogOut.js
@@ -2,22 +2,39 @@ import React from 'react';
 import { auth } from '../firebaseConfig';
 import { signOut } from 'firebase/auth';
 import { Button } from 'react-bootstrap';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const LogOut = () => {
+    const navigate = useNavigate();
     const handleLogOut = () => {
         signOut(auth)
             .then(() => {
-                console.log('User logged out');
+                
+                navigate('../', { replace: true });
             })
             .catch((error) => {
                 console.error('Error logging out:', error);
             });
     };
 
+    const handleNo = () => {
+        
+            navigate('../user/' + auth.currentUser.uid, { replace: true });
+            
+            
+    };
+
     return (
-        <Button variant="secondary" onClick={handleLogOut}>
-            Log Out
+        <div>
+            <p>
+            Are you sure you want to log out?
+            </p>
+            <Button variant="secondary" onClick={handleLogOut}>
+            Yes
         </Button>
+        <Button variant = "secondary" onClick = {handleNo}>No</Button>
+        </div>
+        
     );
 };
 
